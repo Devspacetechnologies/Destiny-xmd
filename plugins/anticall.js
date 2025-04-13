da30b4839eea506e2a0ed16e28a15db5453354fe
Mark <153409985+Devspacetechnologies@users.noreply.github.com>
2025-04-13 12:16:55 +0300

    Update anticall.js

--- plugins/anticall.js
+++ plugins/anticall.js
@@ -9,7 +9,7 @@ const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0
 const text = m.body.slice(prefix.length + cmd.length).trim();
 
   if (cmd === 'anticall') {
-    if (!isCreator) return m.reply("*📛 THIS IS AN OWNER COMMAND*");
+    if (!isCreator) return m.reply("*📛 ONLY DESTINY XMD OWNER CAN USE THIS COMAND*");
     let responseMessage;
 
     if (text === 'on') {
